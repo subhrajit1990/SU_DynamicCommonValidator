7853e6b5ae1a54ecc951ccc63c715ddb0c34154b
subhrajit1990 <subhrajitsahu1990@gmail.com>
2018-10-07 12:39:32 +0530

    validation for react

--- src/components/FirstScreen.js
+++ src/components/FirstScreen.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import CommonValidator from '../utils/CommonValidator';
+import React,{Fragment} from 'react';
+import CV from '../utils/CommonValidator';
+import Timer from '../utils/Timer'
 
 export default class FirstScreen extends React.Component{
 	constructor(props){
@@ -11,27 +12,39 @@ export default class FirstScreen extends React.Component{
 	submit(e){
 		if(e) e.preventDefault();
 		const formData = {};
-		
-  		var tempObj = this.refs;
-  		CommonValidator(tempObj);
+	 	var form = this.refs,
+        form_validator_check = {
+            phone: {
+                verify: CV.nullcheck,
+                message: "Please enter the phone number"
+            },
+            email: {
+                verify: CV.nullcheck,
+                message:"Please enter the correct email"
+            }            
+        };
 
-    // Object.keys(textmpObj).map((a,b)=> {
-    // 	console.log(a);
-    // 	//formData[a] = this.refs[a].value;
-    // })
-		
+  		if(CV.CommonValidator(form,form_validator_check )){
+  			console.log("error true");
+  		}else{
+  			console.log("Error false");
+  		}		
 		  console.log('FORM DATA ', formData);
-};
+	};
 	
 
 	render(){
-		return(			
-			<form onSubmit ={this.submit}>
-      			<input ref="phone" className="phone" type="phone"  name="phone"/>
-            	<input ref="email" className="email" type="email"  name="email"/>
-            	<input ref="text" className="name" type="text" name="firstName" />
-           		<input type="submit" value="Submit"/>
-   			 </form>
+		return(		
+
+			<Fragment>
+				<Timer/>	
+				<form onSubmit ={this.submit}>
+	      			<input ref="phone" className="phone" type="phone"  name="phone"/>
+	            	<input ref="email" className="email" type="email"  name="email"/>
+	            	<input ref="text" className="name" type="text" name="firstName" />
+	           		<input type="submit" value="Submit"/>
+	   			 </form>
+ 			</Fragment>
 		);
 	}
 }
--- src/utils/CommonValidator.js
+++ src/utils/CommonValidator.js
@@ -1,10 +1,45 @@
-export default function CommonValidator(pObj = {}){
-	debugger;
+const CV = {
+    nullcheck: function(s){
+        let err = s.length ? 0 : 5;
+        return err;
 
-    Object.keys(pObj).map((key)=> {
-    	console.log(key + "->" + pObj[key].value);
-    });
-
-	console.log("Cmmon functions "+pObj);
-
+    },
+    valid: function(element, check){
+        if (element.type == 'text' || element.type == 'phone' || element.type == 'email') {
+        return check(element.value);
+    } else {
+        return check(element);
+    }
+    },
+    CommonValidator: function(form,list = {} ,flag){
+        let errorCount = 0,
+        element,
+        n,
+        out;
+        var pThis = this;
+        Object.keys(form).map(function(key, index) {        
+                element = form[key];
+                if (!element.hasAttribute("name")) {
+                    n = element.id, out;
+                } else if (element.hasAttribute("src")) {
+                    n = element.src, out;
+                } else {
+                    n = element.name, out;
+                }
+                if (list[n] && list[n].verify) {
+                    out = pThis.valid(element, list[n].verify);
+                    if (out != 0) {                    
+                        console.log("ERROR"+list[n].message || "Error occurred");
+                        errorCount++;
+                    }
+                }
+          
+        });
+    if (errorCount > 0) {
+        return false;
+    }
+    return true;
+    }
 }
+
+export default CV;
